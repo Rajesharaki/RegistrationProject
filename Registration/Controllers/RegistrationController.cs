b2b91990cba6faaacfca2f6d3dad2d9d7e11dfeb
Rajesharaki <rajesharaki8613@gmail.com>
2022-04-19 22:20:25 +0530

    first commit

---
+++ Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly IRegister _register;
+
+        public RegistrationController(IRegister register)
+        {
+            _register = register;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegistrationModel());
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegistrationModel model)
+        {
+            _register.InsertRegisterRecord(model);
+            return View(model);
+        }
+    }
+}
---
+++ Registration/Repositories/IRegisterRepository.cs
@@ -0,0 +1,32 @@
+﻿using Registration.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration.Repositories
+{
+    public class IRegisterRepository : IRegister
+    {
+        public void InsertRegisterRecord(RegistrationModel model)
+        {
+            using (var con=new SqlConnection(""))
+            {
+                var cmd = new SqlCommand("SpName", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", model.Name);
+                cmd.Parameters.AddWithValue("@Email", model.Email);
+                cmd.Parameters.AddWithValue("@Password", model.Password);
+                cmd.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                cmd.Parameters.AddWithValue("@Gender", model.Gender);
+                cmd.Parameters.AddWithValue("@Language", model.Language);
+                cmd.Parameters.AddWithValue("@Zipcode", model.Zipcode);
+                cmd.Parameters.AddWithValue("@About", model.About);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
